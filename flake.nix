4da12ac0a107f24f634e1d673903c777d29a433b
Yasharth011 <yasharthgupta10@gmail.com>
2025-07-14 09:13:11 +0530

    linking yasmin using find_package()

--- flake.nix
+++ flake.nix
@@ -9,35 +9,33 @@
 
   };
 
-  outputs = { self, nixpkgs , flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: 
-    let pkgs = nixpkgs.legacyPackages.${system};
-    in rec {
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
 
-      packages.yasmin = with pkgs;
-        stdenv.mkDerivation {
-          name = "yasmin";
-          src = fetchFromGitHub {
-            owner = "Yasharth011";
-            repo = "yasmin_UNROS";
-            rev = "f21e30afa122ddc11ff272adf9834e3d18a3b700"; # v3.3.0
-            sha256 = "sha256-QRdBoOWUsm9JXgxIq3BUjgwEqJeJpadXnJLlePhCpW8=";
+        packages.yasmin = with pkgs;
+          stdenv.mkDerivation {
+            name = "yasmin";
+            src = fetchFromGitHub {
+              owner = "Yasharth011";
+              repo = "yasmin_UNROS";
+              rev = "95bcb301809f84e47df8db38c385c4a4318e0ccb"; # v3.3.0
+              sha256 = "sha256-8yKFlNrmOJpScHkj7gHhL8UlP7JZ8aJFUXTrMq4I7gw=";
+            };
+            nativeBuildInputs = [ cmake ];
+            configurePhase = ''
+              	    cd yasmin 
+                          mkdir build && cd build 
+                          cmake .. -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_PREFIX=$out
+            '';
+            buildPhase = "  make\n";
+            installPhase = ''
+              make install
+            '';
           };
-          nativeBuildInputs = [ cmake ];
-          configurePhase = ''
-	    cd yasmin 
-            mkdir build && cd build 
-            cmake .. -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_PREFIX=$out
-          '';
-	  buildPhase = ''
-	    make
-	  ''; 
-          installPhase = ''
-            make install
-          '';
-        };
-      devShells.default =
-        pkgs.mkShell { packages = with pkgs; [ cmake packages.yasmin ]; };
+        devShells.default =
+          pkgs.mkShell { packages = with pkgs; [ cmake packages.yasmin ]; };
 
-    });
+      });
 }
