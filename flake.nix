dec5cd8969ebea1d8163c4df7fa727ca26037a89
Yasharth011 <yasharthgupta10@gmail.com>
2025-07-15 06:37:10 +0530

    librealsense added

--- flake.nix
+++ flake.nix
@@ -34,8 +34,9 @@
               make install
             '';
           };
-        devShells.default =
-          pkgs.mkShell { packages = with pkgs; [ cmake packages.yasmin ]; };
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ cmake packages.yasmin librealsense ];
+        };
 
       });
 }
--- src/mario.cpp
+++ src/mario.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <librealsense2/rs.hpp>
+
 #include <yasmin/state.hpp>
 #include <yasmin/state_machine.hpp>
 
@@ -10,15 +12,64 @@ class Navigate : public yasmin::State {
 public:
   position init_pos;
 
-  Navigate(float init_x, float init_y, float init_yaw)
-      : yasmin::State({"Idle", "Arrow_Detected", "Cone_Detected"}), init_pos{init_x, init_y, init_yaw} {}
+  Navigate() : yasmin::State({"IDLE", "ARROW_DETECTED", "CONE_DETECTED"}) {};
 
   std::string
   execute(std::shared_ptr<yasmin::blackboard::Blackboard> blackboard) override {
-    /* send message to nucleo  */
-    /* fetch position form slam */
-    return "";
+    return "IDLE";
   }
 };
 
-int main() { std::cout << "Hello World"; }
+class Idle : public yasmin::State {
+
+public:
+  position init_pos;
+
+  Idle(float init_x, float init_y, float init_yaw)
+      : yasmin::State(
+            {"NAVIGATE", "ARROW_DETECTED", "CONE_DETECTED", "ROTATE"}),
+        init_pos{init_x, init_y, init_yaw} {};
+
+  std::string
+  execute(std::shared_ptr<yasmin::blackboard::Blackboard> blackboard) override {
+    return "NAVIGATE";
+  }
+};
+
+int main() {
+
+  // Create Pipeline and configure realsense
+  rs2::pipeline pipe;
+  rs2::config cfg;
+
+  // Enable Realsense Streams
+  cfg.enable_stream(RS2_STREAM_DEPTH);
+  cfg.enable_stream(RS2_STREAM_GYRO);
+  cfg.enable_stream(RS2_STREAM_ACCEL);
+  cfg.enable_stream(RS2_STREAM_COLOR);
+  cfg.enable_stream(RS2_STREAM_INFRARED, 2);
+
+  // pipe.start(cfg);
+
+  rs2::pointcloud pc;
+  rs2::points points;
+
+  // Create a state machine
+  auto sm = std::make_shared<yasmin::StateMachine>(
+      std::initializer_list<std::string>{});
+
+  // Add states to the state machine
+  sm->add_state("Navigate", std::make_shared<Navigate>(), {{"IDLE", "Idle"}});
+
+  sm->add_state("Idle", std::make_shared<Idle>(0.0, 0.0, 0.0),
+                {{"NAVIGATE", "Navigate"}});
+
+  // Execute the state machine
+  try {
+    std::string outcome = (*sm.get())();
+  } catch (const std::exception &e) {
+    std::cout << e.what();
+  }
+
+  return 0;
+}
