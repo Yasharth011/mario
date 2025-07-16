a6d30d4b5d47ff1b761981c4c7ad2ccebd6e5cca
Yasharth011 <yasharthgupta10@gmail.com>
2025-07-16 21:15:42 +0530

    mario.cpp : added remaining states + refined code

--- src/mario.cpp
+++ src/mario.cpp
@@ -2,9 +2,9 @@
 
 #include <librealsense2/rs.hpp>
 
+#include <yasmin/logs.hpp>
 #include <yasmin/state.hpp>
 #include <yasmin/state_machine.hpp>
-#include <yasmin/logs.hpp> 
 
 #include <mario.hpp>
 
@@ -13,9 +13,11 @@
 class Navigate : public yasmin::State {
 
 public:
-  position init_pos;
+  serialib serial;
 
-  Navigate() : yasmin::State({"IDLE", "ARROW_DETECTED", "CONE_DETECTED"}) {};
+  Navigate(serialib x)
+      : yasmin::State({"IDLE", "ARROW_DETECTED", "CONE_DETECTED"}),
+        serial(x) {};
 
   std::string
   execute(std::shared_ptr<yasmin::blackboard::Blackboard> blackboard) override {
@@ -26,16 +28,41 @@ public:
 class Idle : public yasmin::State {
 
 public:
-  position init_pos;
+  serialib serial;
 
-  Idle(float init_x, float init_y, float init_yaw)
-      : yasmin::State(
-            {"NAVIGATE", "ARROW_DETECTED", "CONE_DETECTED", "ROTATE"}),
-        init_pos{init_x, init_y, init_yaw} {};
+  Idle(serialib x)
+      : yasmin::State({"NAVIGATE", "ARROW_DETECTED", "CONE_DETECTED"}),
+        serial(x) {};
 
   std::string
   execute(std::shared_ptr<yasmin::blackboard::Blackboard> blackboard) override {
     return "NAVIGATE";
+  }
+};
+
+class Cone_Detected : public yasmin::State {
+
+public:
+  serialib serial;
+
+  Cone_Detected(serialib x) : yasmin::State({"IDLE"}), serial(x) {};
+
+  std::string
+  execute(std::shared_ptr<yasmin::blackboard::Blackboard> blackboard) override {
+    return "IDLE";
+  }
+};
+
+class Arrow_Detected : public yasmin::State {
+
+public:
+  serialib serial;
+
+  Cone_Detected(serialib x) : yasmin::State({"NAVIGATE", "IDLE"}), serial(x) {};
+
+  std::string
+  execute(std::shared_ptr<yasmin::blackboard::Blackboard> blackboard) override {
+    return "Idle";
   }
 };
 
@@ -59,32 +86,44 @@ int main(int argc, cahr *argv[]) {
   rs2::pointcloud pc;
   rs2::points points;
 
-  // serial object 
-  serialib serial; 
+  // serial object
+  serialib nucleo_com;
 
-  // open serial device 
-  char errorOpening = serial.openDevice(SERIAL_PORT, 9600); 
+  // open serial device
+  char errorOpening = serial.openDevice(SERIAL_PORT, 9600);
 
-  // check for errors 
-  if (errorOpening!=1) YASMIN_LOG_WARN("Error opening device");
+  // check for errors
+  if (errorOpening != 1)
+    YASMIN_LOG_ERROR("Error opening device");
 
-  YASMIN_LOG_INFO("Succesful connection to %s\n",SERIAL_PORT);
+  YASMIN_LOG_INFO("Succesful connection to %s\n", SERIAL_PORT);
 
   // Create a state machine
   auto sm = std::make_shared<yasmin::StateMachine>(
       std::initializer_list<std::string>{});
 
   // Add states to the state machine
-  sm->add_state("Navigate", std::make_shared<Navigate>(), {{"IDLE", "Idle"}});
+  sm->add_state("Navigate", std::make_shared<Navigate>(nucleo_com),
+                {{"IDLE", "Idle"},
+                 {"ARROW_DETECTED", "Arrow_Detected"},
+                 {"CONE_DETECTED", "Cone_Detected"}});
 
-  sm->add_state("Idle", std::make_shared<Idle>(0.0, 0.0, 0.0),
-                {{"NAVIGATE", "Navigate"}});
+  sm->add_state("Idle", std::make_shared<Idle>(nucleo_com),
+                {{"NAVIGATE", "Navigate"},
+                 {"ARROW_DETECTED", "Arrow_Detected"},
+                 {"CONE_DETECTED", "Cone_Detected"}});
+
+  sm->add_state("Arrow_Detected", std::make_shared<Arrow_Detected>(nucleo_com),
+                {{"IDLE", "Idle"}, {"NAVIAGE", "Navigate"}});
+
+  sm->add_state("Cone_Detected", std::make_shared<Cone_Detected()>(nucleo_com),
+                {{"IDLE", "Idle"}});
 
   // Execute the state machine
   try {
     std::string outcome = (*sm.get())();
   } catch (const std::exception &e) {
-    std::cout << e.what();
+    YASMIN_LOG_ERROR(e.what())
   }
 
   return 0;
