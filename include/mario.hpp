bf214784fe84df28b2719b6c22dda8c10acc5be0
Yasharth011 <yasharthgupta10@gmail.com>
2025-07-26 18:39:49 +0530

    mario : Thread SafeQueue for vedio processing

--- include/mario.hpp
+++ include/mario.hpp
@@ -1,10 +1,46 @@
 # ifndef MARIO_HPP
 # define MARIO_HPP
 
+#include <mutex>
 struct position {
 	float x_coord; 
 	float y_coord; 
 	float yaw; 
+};
+
+template <typename T> 
+class SafeQueue { 
+	public: 
+		SafeQueue() : q(), m(), c() {}
+
+		void enqueue(T t) { 
+			std::lock_guard<std::mutex> lock(m); 
+			q.push(t); 
+			c.notify_one();
+		}
+
+		bool dequeue(T &t){ 
+			std::unique_lock<std::mutex> lock(m); 
+			while(q.empty()) { 
+				if (finished) return false; 
+				c.wait(lock); 
+			}
+			t = q.front(); 
+			q.pop(); 
+			return true; 
+		}
+
+		void setFinished(){ 
+			std::lock_guard<std::mutex> lock(m); 
+			finished = true; 
+			c.notify_all(); 
+		}
+
+	private: 
+		std::queue<T> q; 
+		mutable std::mutex m; 
+		std::condition_variable c; 
+		bool finished = false; 
 };
 
 # endif
