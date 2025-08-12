46f2ae7492f3ce160b6dbfd4a214604d8c7edd93
Yasharth011 <yasharthgupta10@gmail.com>
2025-08-12 14:33:21 +0530

    mario : Formatting

--- src/mario.cpp
+++ src/mario.cpp
@@ -28,6 +28,7 @@ namespace po = boost::program_options;
 
 int main(int argc, char *argv[]) {
 
+  // Command Line Options
   po::options_description desc("Allowed Options");
 
   desc.add_options()
@@ -47,11 +48,36 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+  tf::Executor executor; // creating exectutor  
+  tf::Taskflow taskflow; // & taskflow graph obj
+  rs2::pipeline pipe; // creating rs
+  rs2::config cfg;   // pipeline & cfg obj 
+  rs2::pointcloud pc; // rs pointcloud 
+  rs2::points points; // rs point obj
+  cv::VideoCapture 
+	  capture(vm["cam"].as<int>(), cv::CAP_V4L2); // cam obj for YOLO 
+  const std::string model_path = 
+	  vm["model"].as<std::string>(); // YOLO model path 
+  const std::string labels_path = 
+	  vm["labels"].as<std::string>(); // class label path 
+  YOLO8Detector detector(model_path, labels_path, true); // create onnx inference obj 
+  SafeQueue<cv::Mat> frameQueue; // queue to store raw frames
+  SafeQueue<std::pair<int, cv::Mat>> processedQueue; // queue to store processed frames
+  std::vector<std::string> class_names =
+	  utils::getClassNames(labels_path); // load class names 
+  serialib nucleo_com; // nucleo com obj 
+  std::string SERIAL_PORT = 
+	  vm["serial"].as<std::string>(); // serial port to nucleo 
+  auto sm = std::make_shared<yasmin::StateMachine>(
+      std::initializer_list<std::string>{"CONE_DETECTED"}); // state machine obj 
+  auto blackboard = 
+      std::make_shared<yasmin::blackboard::Blackboard>(); // init blackboard 
+
+  /* CONFIGURING PERIPHERALS */
+
   YASMIN_LOG_INFO("Configuring rover peripherals...");
 
-  // Create Pipeline and configure realsense
-  rs2::pipeline pipe;
-  rs2::config cfg;
+  /* CONFIGURING REALSENSE */
 
   // Enable Realsense Streams
   cfg.enable_stream(RS2_STREAM_DEPTH);
@@ -69,14 +95,8 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
-  rs2::pointcloud pc;
-  rs2::points points;
-
-  // serial object
-  serialib nucleo_com;
-
-  std::string SERIAL_PORT = vm["serial"].as<std::string>();
-
+  /* CONFIGURING NUCLEO COM */
+  
   // open serial device
   char errorOpening = nucleo_com.openDevice(SERIAL_PORT.c_str(), 9600);
 
@@ -85,29 +105,40 @@ int main(int argc, char *argv[]) {
     YASMIN_LOG_ERROR("Nucleo : Error opening device");
     return -1;
   }
-
   YASMIN_LOG_INFO("Succesful connection to %s\n", SERIAL_PORT.c_str());
 
-  // open camera for object detection
-  cv::VideoCapture capture(vm["cam"].as<int>(), cv::CAP_V4L2); // default camera for now
-
+  /* CONFIGURING CAM */
+  
   // check for errors
   if (!capture.isOpened()) {
     YASMIN_LOG_ERROR("Cam : Error opening device");
     return -1;
   }
+  YASMIN_LOG_INFO("Succesful connection to %s\n", SERIAL_PORT.c_str());
 
-  // load YOLOV8 model
-  const std::string model_path = vm["model"].as<std::string>();
-  const std::string labels_path = vm["labels"].as<std::string>();
-  YOLO8Detector detector(model_path, labels_path, true);
+  /* CONFIGURING STATE MACHINE */
 
-  // Thread-safe queues for vedio processing
-  SafeQueue<cv::Mat> frameQueue;
-  SafeQueue<std::pair<int, cv::Mat>> processedQueue;
+  // Add states to the state machine
+  sm->add_state("Navigate", std::make_shared<Navigate>(nucleo_com),
+                {{"IDLE", "Idle"},
+                 {"ARROW_DETECTED", "Arrow_Detected"},
+                 {"CONE_DETECTED", "Cone_Detected"}});
 
-  // Get Class Names 
-  std::vector<std::string> class_names = utils::getClassNames(labels_path);
+  sm->add_state("Idle", std::make_shared<Idle>(nucleo_com),
+                {{"NAVIGATE", "Navigate"},
+                 {"ARROW_DETECTED", "Arrow_Detected"},
+                 {"CONE_DETECTED", "Cone_Detected"}});
+
+  sm->add_state("Arrow_Detected", std::make_shared<Arrow_Detected>(nucleo_com),
+                {{"IDLE", "Idle"}, {"NAVIGATE", "Navigate"}});
+
+  sm->add_state("Cone_Detected", std::make_shared<Cone_Detected>(nucleo_com),
+                {{"IDLE", "Idle"}});
+
+  // set start of FSM as IDLE STATE
+  sm->set_start_state("Idle");
+
+  /* TASK FLOW GRAPH */
 
   // Threads for detection
   // Capture Thread
@@ -148,38 +179,6 @@ int main(int argc, char *argv[]) {
       cv::waitKey(1);
     }
   });
-
-  // Decalre status variable in the blackboard
-  auto blackboard = std::make_shared<yasmin::blackboard::Blackboard>();
-  blackboard->set<bool>("right_arrow", false);
-  blackboard->set<bool>("left_arrow", false);
-  blackboard->set<bool>("cone", false);
-
-  // Create a state machine
-  auto sm = std::make_shared<yasmin::StateMachine>(
-      std::initializer_list<std::string>{"CONE_DETECTED"});
-
-
-  // Add states to the state machine
-  sm->add_state("Navigate", std::make_shared<Navigate>(nucleo_com),
-                {{"IDLE", "Idle"},
-                 {"ARROW_DETECTED", "Arrow_Detected"},
-                 {"CONE_DETECTED", "Cone_Detected"}});
-
-  sm->add_state("Idle", std::make_shared<Idle>(nucleo_com),
-                {{"NAVIGATE", "Navigate"},
-                 {"ARROW_DETECTED", "Arrow_Detected"},
-                 {"CONE_DETECTED", "Cone_Detected"}});
-
-  sm->add_state("Arrow_Detected", std::make_shared<Arrow_Detected>(nucleo_com),
-                {{"IDLE", "Idle"}, {"NAVIGATE", "Navigate"}});
-
-  sm->add_state("Cone_Detected", std::make_shared<Cone_Detected>(nucleo_com),
-                {{"IDLE", "Idle"}});
-
-  // set start of FSM as IDLE STATE
-  sm->set_start_state("Idle");
-
 
 
   // state machine thread
