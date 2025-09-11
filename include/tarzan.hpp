96b5dbdad458cc153e6730f9d6b966a1b410b51c
Yasharth011 <yasharthgupta10@gmail.com>
2025-09-11 15:40:49 +0530

    tarzan : refactor + cobs encode error fix

--- include/tarzan.hpp
+++ include/tarzan.hpp
@@ -9,34 +9,33 @@
 
 using namespace boost::asio;
 
-class Tarzan {
+/* extra msg struct for Tarzan msg */
+struct inverse_msg {
+  double turn_table;
+  double first_link;
+  double second_link;
+  double pitch;
+  double roll;
+  double x;
+  double y;
+  double z;
+};
 
-private:
-  /* msg struct for Tarzan msg */
-  struct inverse_msg {
-    double turn_table;
-    double first_link;
-    double second_link;
-    double pitch;
-    double roll;
-    double x;
-    double y;
-    double z;
-  };
+struct imu_data {
+  double accel[3];
+  double gyro[3];
+  double mag[3];
+  double gyro_offset[3];
+};
 
-  struct imu_data {
-    double accel[3];
-    double gyro[3];
-    double mag[3];
-    double gyro_offset[3];
-  };
+struct imu_msg {
+  struct imu_data baseLink;
+  struct imu_data firstLink;
+  struct imu_data secondLink;
+  struct imu_data differential;
+};
 
-  struct imu_msg {
-    struct imu_data baseLink;
-    struct imu_data firstLink;
-    struct imu_data secondLink;
-    struct imu_data differential;
-  };
+namespace tarzan {
 
   struct DiffDriveTwist {
     float linear_x;
@@ -45,42 +44,28 @@ private:
 
   enum msg_type { AUTONOMOUS, INVERSE };
 
-  std::mutex write_mtx; // mutex to guard async write
-  std::mutex error_mtx; // mutex to gaurd error in async write
-  bool writeLocked = false;
-  std::condition_variable writeCV;
-  int err;
-
-public:
-  serial_port serial; // serial port
-  std::string port; 
-  unsigned int baudrate;
 
   enum Error : uint8_t { WriteSuccess = 0, CobsEncodeError, AsioWriteError };
 
   // msg to write
   struct tarzan_msg {
     struct DiffDriveTwist cmd;
-    struct inverse_msg inv;
-    struct imu_msg imu;
-    enum msg_type type;
+    // struct inverse_msg inv;
+    // struct imu_msg imu;
+    // enum msg_type type;
     uint32_t crc;
   };
-
-  Tarzan(io_context &io, const std::string port, unsigned int baudrate)
-      : serial(io, port), port(port), baudrate(baudrate) {
-  }
 
   void asyncWriteHandler(const boost::system::error_code &error,
                          std::size_t bytes_transferred);
 
-  Error asyncWrite(const uint8_t msg[]);
+  Error asyncWrite(serial_port *serial, const uint8_t msg[]);
 
-  Error write_msg(const tarzan_msg &msg);
+  Error write_msg(serial_port *serial, const tarzan_msg &msg);
 
-  void close();
+  void close(serial_port *serial);
 
-  int open();
+  serial_port *open(io_context &io, const std::string port, unsigned int baudrate);
 
   uint32_t crc32_ieee(const uint8_t *data, size_t len);
 
--- src/tarzan.cpp
+++ src/tarzan.cpp
@@ -1,13 +1,21 @@
 #include <cobs.h>
 #include <cstddef>
 #include <cstdint>
+#include <imu.h>
 
 #include "tarzan.hpp"
 
-constexpr size_t TARZAN_MSG_LEN = sizeof(Tarzan::tarzan_msg) + 2;
+namespace tarzan {
+constexpr size_t TARZAN_MSG_LEN = sizeof(tarzan_msg) + 2;
 
-void Tarzan::asyncWriteHandler(const boost::system::error_code &error,
-                               std::size_t bytes_transferred) {
+std::mutex write_mtx; // mutex to guard async write
+std::mutex error_mtx; // mutex to gaurd error in async write
+bool writeLocked = false;
+std::condition_variable writeCV;
+int err;
+
+void asyncWriteHandler(const boost::system::error_code &error,
+                       std::size_t bytes_transferred) {
   std::unique_lock<std::mutex> elk(error_mtx);
   if (error) {
     err = error.value();
@@ -21,7 +29,7 @@ void Tarzan::asyncWriteHandler(const boost::system::error_code &error,
                         // continue
 }
 
-Tarzan::Error Tarzan::asyncWrite(const uint8_t msg[TARZAN_MSG_LEN]) {
+Error asyncWrite(serial_port *serial, const uint8_t msg[TARZAN_MSG_LEN]) {
 
   std::unique_lock<std::mutex> lock(write_mtx);
 
@@ -32,63 +40,66 @@ Tarzan::Error Tarzan::asyncWrite(const uint8_t msg[TARZAN_MSG_LEN]) {
 
   lock.unlock();
 
-  async_write(Tarzan::serial, buffer(msg, TARZAN_MSG_LEN),
-              [this](const boost::system::error_code &error,
-                     std::size_t bytes_transferred) {
+  async_write(*serial, buffer(msg, TARZAN_MSG_LEN),
+              [](const boost::system::error_code &error,
+                 std::size_t bytes_transferred) {
                 asyncWriteHandler(error, bytes_transferred);
               });
   std::unique_lock<std::mutex> elk(error_mtx);
 
   if (!err)
-    return Tarzan::Error::WriteSuccess;
+    return Error::WriteSuccess;
   else
-    return Tarzan::Error::AsioWriteError;
+    return Error::AsioWriteError;
 }
 
-Tarzan::Error Tarzan::write_msg(const Tarzan::tarzan_msg &msg) {
+Error write_msg(serial_port *serial, const tarzan_msg &msg) {
 
   uint8_t buffer[TARZAN_MSG_LEN];
 
   if (auto result = cobs_encode(
-          reinterpret_cast<void *>(buffer), sizeof(TARZAN_MSG_LEN),
-          reinterpret_cast<const void *>(&msg), sizeof(TARZAN_MSG_LEN));
+          reinterpret_cast<void *>(buffer), TARZAN_MSG_LEN,
+          reinterpret_cast<const void *>(&msg), sizeof(struct tarzan_msg));
       result.status != COBS_ENCODE_OK) {
-    return Tarzan::Error::CobsEncodeError;
+    return Error::CobsEncodeError;
   }
 
   buffer[TARZAN_MSG_LEN - 1] = 0x00;
 
-  return Tarzan::asyncWrite(buffer);
+  return asyncWrite(serial, buffer);
 }
 
-void Tarzan::close() {
-  if (!Tarzan::serial.is_open())
+void close(serial_port *serial) {
+  if (!serial->is_open())
     return;
-  Tarzan::serial.cancel(); // cancel all pending async processes
-  Tarzan::serial.close();  // close the serial port
+  serial->cancel(); // cancel all pending async processes
+  serial->close();  // close the serial port
 }
 
-int Tarzan::open() {
+serial_port *open(io_context &io, const std::string port,
+                  unsigned int baudrate) {
+
+  // create boost serial object
+  serial_port *serial = new serial_port(io);
 
   // close if port was already opened
-  if (serial.is_open())
-    close();
+  if (serial->is_open())
+    serial->close();
 
   try {
-    serial.open(Tarzan::port);
-    serial.set_option(serial_port_base::baud_rate(Tarzan::baudrate));
-    return 1;
+    serial->open(port);
+    serial->set_option(serial_port_base::baud_rate(baudrate));
+    return serial;
   } catch (...) {
-    return 0;
+    return nullptr;
   }
 }
 
-uint32_t Tarzan::crc32_ieee(const uint8_t *data, size_t len) {
+uint32_t crc32_ieee(const uint8_t *data, size_t len) {
   return crc32_ieee_update(0x0, data, len);
 }
 
-uint32_t Tarzan::crc32_ieee_update(uint32_t crc, const uint8_t *data,
-                                   size_t len) {
+uint32_t crc32_ieee_update(uint32_t crc, const uint8_t *data, size_t len) {
   /* crc table generated from polynomial 0xedb88320 */
   static const uint32_t table[16] = {
       0x00000000U, 0x1db71064U, 0x3b6e20c8U, 0x26d930acU,
@@ -109,20 +120,21 @@ uint32_t Tarzan::crc32_ieee_update(uint32_t crc, const uint8_t *data,
   return (~crc);
 }
 
-const char *get_error(enum Tarzan::Error err) {
+const char *get_error(enum Error err) {
   switch (err) {
-  case Tarzan::Error::WriteSuccess:
+  case Error::WriteSuccess:
     return "Serial Write Successfull";
     break;
-  case Tarzan::Error::CobsEncodeError:
+  case Error::CobsEncodeError:
     return "Cobs Encode Error";
     break;
-  case Tarzan::Error::AsioWriteError:
+  case Error::AsioWriteError:
     return "Asio Serial Write Error";
     break;
   };
   return "Undefined Error";
 }
+} // namespace tarzan
 
 #ifdef TARZAN_TEST_CPP
 #include <iostream>
@@ -131,17 +143,20 @@ int main(int argc, char *argv[]) {
 
   std::string port = argv[1];
   boost::asio::io_context io;
-  Tarzan nucleo(io, port, 9600);
+  boost::asio::serial_port *nucleo = tarzan::open(io, port, 9600);
 
-  Tarzan::tarzan_msg msg;
+  tarzan::tarzan_msg msg;
   msg.cmd.linear_x = 1.0;
   msg.cmd.angular_z = 0.5;
-  msg.crc = nucleo.crc32_ieee(
-      (uint8_t *)&msg, sizeof(struct Tarzan::tarzan_msg) - sizeof(msg.crc));
+  msg.crc = tarzan::crc32_ieee(
+      (uint8_t *)&msg, sizeof(struct tarzan::tarzan_msg) - sizeof(msg.crc));
 
   std::cout << "crc = " << msg.crc;
-  std::cout << "writing message" << std::endl;
 
-  nucleo.write_msg(msg);
+  std::string err = get_error(tarzan::write_msg(nucleo, msg));
+
+  std::cout<<"Error : "<<err; 
+
+  tarzan::close(nucleo);
 }
 #endif
