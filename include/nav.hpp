6a120eca1b542f75799f23958a8cc30487024c23
Yasharth011 <yasharthgupta10@gmail.com>
2025-09-27 02:56:37 +0530

    nav: changed variables to inline causing multiple def error otherwise

--- include/nav.hpp
+++ include/nav.hpp
@@ -13,30 +13,37 @@
 
 namespace nav {
 
-struct navContext{
-	Gridmap gridmap;
-	QuadtreeNode lowQuadtree; 
-	QuadtreeNode midQuadtree; 
-	QuadtreeNode highQuadtree; 
-	Node start; 
-	Node goal; 
-	Node current_start; 
-	Node current_goal;
-	Node final_goal; 
-	std::vector<Node> full_path; 
-	std::vector<std::pair<int, int>> visited_node; 
-	std::set<std::pair<int, int>> failed_goals; 
-	std::vector<Node> dense_path;
+inline int batch_threshold = 1; 
+inline Point center;
+inline float rootSize;
+
+struct navContext {
+  Gridmap gridmap;
+  QuadtreeNode lowQuadtree;
+  QuadtreeNode midQuadtree;
+  QuadtreeNode highQuadtree;
+  Node start;
+  Node goal;
+  Node current_start;
+  Node current_goal;
+  Node final_goal;
+  std::vector<Node> full_path;
+  std::vector<std::pair<int, int>> visited_node;
+  std::set<std::pair<int, int>> failed_goals;
+  std::vector<Node> dense_path;
+
+  navContext()
+      : gridmap(), lowQuadtree(center, rootSize, 1),
+        midQuadtree(center, rootSize, 1), highQuadtree(center, rootSize, 1),
+        start(0, 0), goal(0, 0), current_start(0, 0), current_goal(0, 0), final_goal(0, 0),
+        full_path(), visited_node(), failed_goals(), dense_path() {}
 };
-
-float grid_resolution = 0.001f;
-int batch_threshold = 1; 
-
 
 std::vector<Eigen::Vector3f>
 processPointCloud(std::vector<Eigen::Vector3f> raw_points);
 
-void updateMaps(const std::vector<Eigen::Vector3f> &points);
+void updateMaps(struct navContext *ctx, struct Slam_Pose &pose,
+                const std::vector<Eigen::Vector3f> &points);
 
 std::vector<Node> prunePath(const std::vector<Node>& path);
 
